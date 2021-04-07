cab713f325ae121e94913999be462014d4cf12bb
Anar <sadiglianar@gmail.com>
2021-04-07 23:35:14 +0400

    todo-item components added

---
+++ src/app/todos/todo-item/todo-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.scss']
+})
+export class TodoItemComponent implements OnInit {
+
+  @Input() todoText: string;
+  @Input() completed: boolean;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
